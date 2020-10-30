ecd2fa0d0af49ae73ac76e2600b4197aa855d51d
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2020-10-30 16:10:57 -0400

    Add a configuration option for logging the job properties. Use PluginConfiguration annotation in-place of the now deprecated ExtensionConfiguration. Remove code that was setting the credentials option as well as the property in the pre-configured job definition.

--- pulumi-orca/src/main/kotlin/com/pulumi/plugin/preconfigured/PulumiPlugin.kt
+++ pulumi-orca/src/main/kotlin/com/pulumi/plugin/preconfigured/PulumiPlugin.kt
@@ -6,6 +6,7 @@ import com.netflix.spinnaker.orca.clouddriver.config.KubernetesPreconfiguredJobP
 import org.pf4j.Extension
 import org.pf4j.Plugin
 import org.pf4j.PluginWrapper
+import org.slf4j.LoggerFactory
 
 
 class PulumiPlugin(wrapper: PluginWrapper): Plugin(wrapper) {
@@ -21,19 +22,22 @@ class PulumiPlugin(wrapper: PluginWrapper): Plugin(wrapper) {
 
 @Extension
 class PulumiPreConfiguredStage(val pluginSdks: PluginSdks, val configuration: PluginConfig) : PreconfiguredJobConfigurationProvider {
+    private val logger = LoggerFactory.getLogger(PulumiPreConfiguredStage::class.java)
+
     override fun getJobConfigurations(): List<KubernetesPreconfiguredJobProperties> {
         val jobProperties = pluginSdks.yamlResourceLoader().loadResource("com/pulumi/plugin/preconfigured/pulumi.yaml", KubernetesPreconfiguredJobProperties::class.java)
         if (!configuration.account.isNullOrEmpty()) {
             jobProperties.account = configuration.account
-        }
-        if (!configuration.credentials.isNullOrEmpty()) {
-            jobProperties.credentials = configuration.credentials
         }
         if (!configuration.namespace.isNullOrEmpty()) {
             jobProperties.manifest.metadata.namespace = configuration.namespace
         }
         if (!configuration.sshConfigPath.isNullOrEmpty()) {
             jobProperties.manifest.spec.template.spec.containers[0].addVolumeMountsItem(io.kubernetes.client.models.V1VolumeMount().mountPath(configuration.sshConfigPath))
+        }
+
+        if (configuration.logJobProperties == true) {
+            logger.info("Job properties: ${pluginSdks.serde().toJson(jobProperties)}");
         }
         return arrayListOf(jobProperties)
     }
