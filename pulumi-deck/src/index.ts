9d2795465456c7ff8c82b29b33aded8c790cc62a
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2021-01-29 18:23:14 -0800

    Update pulumi-deck/src/index.ts

--- pulumi-deck/src/index.ts
+++ pulumi-deck/src/index.ts
@@ -3,8 +3,6 @@ import {
   IDeckPlugin,
 } from '@spinnaker/core';
 
-import { RunJobExecutionDetails } from "@spinnaker/kubernetes/lib/pipelines/stages/runJob/RunJobExecutionDetails";
-
 import { PulumiRunJobExecutionDetails } from './PulumiRunJobExecutionDetails';
 import { PulumiStageConfig } from './PulumiStageConfig';
 
